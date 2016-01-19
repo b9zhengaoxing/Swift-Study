c743c25711b99c910bced9396fa5cc3707021efd
b9zhengaoxing <1164834201@qq.com>
2016-01-19 13:25:56 +0800

    第一个Demo
    playground的helloWorld

---
+++ HelloWorld/MyPlayground.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+/*:  富文本注释
+### 富文本注释
+1. 插入图片
+2. 插入颜色
+3. 使用Markdown语法注释
+*/
+
+var str = "Hello world"
+let image = UIImageView(image:[#Image(imageLiteral: "PGH_pg_editor_2x.png")#])
+image.tintColor = [#Color(colorLiteralRed: 0.6000000237999999, green: 0.400000006, blue: 0.200000003, alpha: 1)#]
+
+
